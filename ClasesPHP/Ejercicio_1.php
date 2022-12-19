c698773394f9f2538aa6489c9ae77e0f1e035178
Jesus <isc.jesusalzati@gmail.com>
2022-12-19 15:03:05 -0600

    Add PHP Files

---
+++ ClasesPHP/Ejercicio_1.php
@@ -0,0 +1,32 @@
+<?php
+    //Este es un comentario de una sola linea
+
+    /*Este es un comentario
+    multilinea
+    */
+    
+    //Imprime una o mas cadenas de texto, toda instrucción de PHP debe terminar con ;
+    echo "Hello World";
+    
+    //Pueden colocarse elementos HTML
+    echo ("<h1>Hola</h1>");
+ 
+    //Imprime información legible
+    print_r("Hello World with print_r");
+
+    //Imprime una cadena
+    print("Hello world con print");
+
+    //Forma de declarar una variable con $, en este caso la variable a tiene el valor de 1.
+    $a = 1;
+
+    /*Podemos imprimir el valor de la variable sin indicar estríctamente su valor, el resultado será 1.
+    echo imprime el valor que contiene a.
+    */
+
+    echo $a;
+
+     //Tipos de datos: Numérico, Cadena, Booleano
+
+
+?>
